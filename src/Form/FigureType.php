a61ed19bf4d1ad46125f72aec83d87a550542ed7
Florence Grisot <flo.roy@gmail.com>
2021-04-16 09:11:51 +0200

    #3

--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Entity\Figure;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -22,6 +24,19 @@ class FigureType extends AbstractType
             -> add('category', EntityType::class,[
                 'class'=> Category::class,
                 'choice_label'=>'title'
+            ])
+            ->add('figureImage', FileType::class, [
+                'label' => 'Image de la figure',
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048K',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image file',
+                    ])],
             ]);
     }
 
